e98484735567f3829d2095a45822f5585670833d
IngridC30 <93414155+IngridC30@users.noreply.github.com>
2021-10-30 15:25:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,18 +136,18 @@ def start_scheduler():
     scheduler = BackgroundScheduler()
 
     # run every 10 minute
-    scheduler.add_job(job_wakeup, 'cron', minute='*/10')
+    #scheduler.add_job(job_wakeup, 'cron', minute='*/10')
 
     # 每天早上6:30執行
     #scheduler.add_job(job_function2, 'cron', hour='6', minute='30')
     # 每分鐘
-    #scheduler.add_job(job_function2, 'cron', minute='*/1')
+    scheduler.add_job(job_function2, 'cron', minute='*/1')
     
     # 每天15:30執行 - 注意 Heroku 時差設定
     scheduler.add_job(job_function2, 'cron', hour='15', minute='30')    
 
     # every 20 minute執行
-    scheduler.add_job(job_function3, 'cron', minute='*/20')      
+    #scheduler.add_job(job_function3, 'cron', minute='*/20')      
 
     # start the scheduler
     scheduler.start()
